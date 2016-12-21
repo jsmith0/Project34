cca923ef7674e16b54d3f49b69ba57bec6a54261
js456 <justins4@comcast.net>
2016-12-21 10:31:45 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a [] [] = new int [3] [4];
+        int b [] [] = new int [4] [2];
+
+    }
+}
---
+++ src/com/company/MatrixMult.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by js456 on 12/21/16.
+ */
+public class MatrixMult {
+
+    public static int mult (int a [], int b []){
+        int [][] w = new int[a.length][b[0].length];
+        System.out.println(w);
+    }
+}
